a7525a3ad7c4cccba2761ae0ce83799a3ee84afc
Vyacheslav001 <leo-slava@mail.ru>
2022-05-16 23:51:38 +0300

    Этап 1 завершен.
    1. Исправлена ошибка бэкстэка в методе setFragment()
    
    Навели красоту

--- app/src/main/java/com/fun/joke/jokes/MainActivity.kt
+++ app/src/main/java/com/fun/joke/jokes/MainActivity.kt
@@ -9,16 +9,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        if (savedInstanceState == null)
-            changeFragment(MainFragment.newInstance())
-//            supportFragmentManager.beginTransaction()
-//                .replace(R.id.fragment_container, MainFragment.newInstance()).commit()
+        if (savedInstanceState == null) {
+            setFragment(MainFragment.newInstance())
+        }
     }
 
-    fun changeFragment(fragment: Fragment) {
+    private fun setFragment(fragment: Fragment) {
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragment_container, fragment)
-            .addToBackStack("")
             .commit()
     }
 }
